2a6b711c4677fda80fb3916b981e9080cf4647b4
jlvn <julianzabala1@gmail.com>
2014-12-15 18:43:07 -0800

    Update _header.php

--- PVault/views/_header.php
+++ PVault/views/_header.php
@@ -4,17 +4,75 @@
     <meta charset="UTF-8">
     <meta http-equiv="refresh" content="300;url=index.php?logout">
     <title>PVault</title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:200' rel='stylesheet' type='text/css'>
     <style type="text/css">
         /* just for the demo */
-        body {
-            font-family: Arial, sans-serif;
-            font-size: 12px;
-            margin: 10px;
+        
+        /*Global styling for all elements*/
+        body, content {
+            font-family: 'Raleway', sans-serif;
+            font-size: 14px;
+            margin: 0;
+            padding: 0;
+            color: black;
         }
-        label {
-            position: relative;
-            vertical-align: middle;
-            bottom: 1px;
+
+
+
+        /*Start hmenu styling.  This is just the navigation bar.*/
+        #hmenu {
+            background-color: #ededed;
+            margin: 0 0 0 0;
+            padding: 20 20 20 20;
+            width: 95%;
+            height: 50px;
+            color: white;
+            text-transform: uppercase;
+        }
+        #hmenu ul {
+            display: ;
+            list-style-type: none;
+            text-align: right;
+        }
+        #hmenu li {
+            display: inline;
+            text-transform: uppercase;
+            font-size: 24px;
+        }
+        #hmenu a:link, a:visited, a:hover {
+            color: white;
+            text-decoration: none;
+            padding: 29 5 25 5;
+        }
+        #hmenu a:hover {
+            background-color: #e4e4e4;
+        }
+        /*End of hmenu styling */
+
+        #left, #right {
+            display: inline;
+            padding: 15px;
+            color: black;
+        }
+        #left {
+            float: left;
+            width: 40%;
+        }
+        #right {
+            width: auto;
+            float: right;
+            padding-right: 20px;
+        }
+        #external-links a:link {
+            text-transform: uppercase;
+            text-decoration: none;
+            color: black;
+        }
+        #external-links a:hover {
+            color: e4e4e4;
+            text-transform: uppercase;
+            padding: 0;
         }
         input[type=text],
         input[type=password],
@@ -24,10 +82,16 @@
             margin-bottom: 15px;
         }
         input[type=checkbox] {
+            display:inline;
             margin-bottom: 15px;
         }
+        #registration_form {
+            padding: 20px;
+        }
+        #logged_in {
+            padding: 40px;
+        }
     </style>
-    
     <script src="scripts/popup.js"></script>
     <script src="scripts/confirmationpopup.js"></script>
 </head>
