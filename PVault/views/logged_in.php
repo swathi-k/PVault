6f623ffdd77c4a80b6ea4bedead887ad23ea650d
Swathi Kotturu <kotturuswathi@yahoo.com>
2014-12-15 19:50:12 -0800

    Removed debugging print statements

--- PVault/views/logged_in.php
+++ PVault/views/logged_in.php
@@ -1,7 +1,8 @@
 <?php include('_header.php'); ?>
 
-<div id="logged_in"><center><h2>Hello!  <?php echo WORDING_YOU_ARE_LOGGED_IN_AS . $_SESSION['user_name'] . ".</h2><br />"; ?>
-Your user ID is <?php echo $_SESSION['user_id'] . ".<br />"; ?></center>
+<div id="logged_in"><center><h2>Hello!  
+<?php echo WORDING_YOU_ARE_LOGGED_IN_AS . $_SESSION['user_name'] . ".</h2><br />"; ?>
+</center>
 <p>Ready to upload?</br>
 <form action="index.php" method="post" enctype="multipart/form-data">
 	<label for="file">Filename:</label>
