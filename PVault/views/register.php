3f3f7d7706c7a8a791ea0dd54ca473d8ba4b7e3d
jlvn <julianzabala1@gmail.com>
2014-12-15 18:47:02 -0800

    Styled the form

--- PVault/views/register.php
+++ PVault/views/register.php
@@ -1,26 +1,30 @@
-<?php include('_header.php'); ?>
 <?php include('navbar_not_logged_in.php'); ?>
+<?php include('_header.php'); ?>
+
 <!-- show registration form, but only if we didn't submit already -->
 <?php if (!$registration->registration_successful && !$registration->verification_successful) { ?>
-<form method="post" action="register.php" name="registerform">
+
+<Center><h1>Create your own PVault account.</h1></center>
+<form id="registration_form" class="pure-form pure-form-stacked" method="post" action="register.php" name="registerform">
+    
     <label for="user_name"><?php echo WORDING_REGISTRATION_USERNAME; ?></label>
-    <input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required />
+    <input id="user_name" type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" required /></BR>
 
     <label for="user_email"><?php echo WORDING_REGISTRATION_EMAIL; ?></label>
-    <input id="user_email" type="email" name="user_email" required />
+    <input id="user_email" type="email" name="user_email" required /></BR>
 
     <label for="user_password_new"><?php echo WORDING_REGISTRATION_PASSWORD; ?></label>
-    <input id="user_password_new" type="password" name="user_password_new" pattern=" ^.*(?=.{8,20})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$ " required autocomplete="off" />
+    <input id="user_password_new" type="password" name="user_password_new" pattern=" ^.*(?=.{8,20})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$ " required autocomplete="off" /></BR>
 
     <label for="user_password_repeat"><?php echo WORDING_REGISTRATION_PASSWORD_REPEAT; ?></label>
-    <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=" ^.*(?=.{8,20})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$ " required autocomplete="off" />
+    <input id="user_password_repeat" type="password" name="user_password_repeat" pattern=" ^.*(?=.{8,20})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$ " required autocomplete="off" /></BR>
 
-    <img src="tools/showCaptcha.php" alt="captcha" />
+    <img src="tools/showCaptcha.php" alt="captcha" /></BR>
 
     <label><?php echo WORDING_REGISTRATION_CAPTCHA; ?></label>
-    <input type="text" name="captcha" required />
+    <input type="text" name="captcha" required /></BR>
 
-    <input type="submit" name="register" value="<?php echo WORDING_REGISTER; ?>" />
+    <button type="submit" class="pure-button pure-input-1-4 pure-button-primary" name="register"  /><?php echo WORDING_REGISTER; ?></button>
 </form>
 <?php } ?>
 
